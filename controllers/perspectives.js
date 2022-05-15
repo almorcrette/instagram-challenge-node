6262101b25ac9f05bbe9c8ef6d5b2eef926b38f7
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-15 19:21:50 +0100

    problem with creating/saving image when submitting image

--- controllers/perspectives.js
+++ controllers/perspectives.js
@@ -1,15 +1,6 @@
 const path = require("path");
 const fs = require('fs');
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-});
-const upload = multer({ storage: storage });
+
 
 
 const Perspective = require("../models/perspectives");
@@ -31,8 +22,6 @@ const PerspectivesController = {
   },
 
   Create: (req, res, next) => {
-    upload.single('perspective');
-
     var obj = {
       name: req.body.name,
       desc: req.body.desc,
--- routes/perspectives.js
+++ routes/perspectives.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+const upload = multer({ storage: storage });
 
 const PerspectivesController = require("../controllers/perspectives");
 
 router.get("/", PerspectivesController.Index);
-router.post("/", PerspectivesController.Create);
+router.post("/", upload.single('perspective'), PerspectivesController.Create);
 router.get("/new", PerspectivesController.New);
 
 
