932f9d3242fdb61e2587e0ad3e7ef13f247ac471
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-15 11:23:58 +0100

    user log-in integration test examined and username for sign-up test drived on frontend

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,17 @@
+// Import User model
 const User = require("../models/user");
 
+// Sets up UsersController.
+// Sets out functions called in the routes
+// wrapped into an object
 const UsersController = {
+
+  // Sets up function to be used in /users/new GET route
   New: (req, res) => {
     res.render("users/new", {});
   },
 
+  // Sets up function to be used in /users POST route
   Create: (req, res) => {
     const user = new User(req.body);
     user.save((err) => {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const express = require("express");
+
+// expressRouter class creates modular, mountable route handlers
+// A Router instance is a complete middleware and routing system
+// Often referred to as a 'mini-app'
+// See: https://expressjs.com/en/guide/routing.html for details
 const router = express.Router();
 
+// Imports functions to be used in users/ routes
+//wrapped into an object UsersController
 const UsersController = require("../controllers/users");
 
+// /users routes
 router.get("/new", UsersController.New);
 router.post("/", UsersController.Create);
 
