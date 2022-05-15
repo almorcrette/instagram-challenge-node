4cbfa0c82ef3ccf896170dad62ad60f05378e97a
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-05-15 11:38:12 +0100

    integration tests added for redirect back to users/new sign up page if signup attempted without any of username, email and password

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -9,4 +9,35 @@ describe("Registration", () => {
 
     cy.url().should("include", "/sessions/new");
   });
+
+  describe("requires username, email and password to be provided", () => {
+    it("fails to register without a username", () => {
+      cy.visit("/users/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+
+      cy.url().should("include", "/users/new");
+    });
+
+    it("fails to register without an email", () => {
+      cy.visit("/users/new");
+      cy.get("#username").type("someone");
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
+
+      cy.url().should("include", "/users/new");
+
+    });
+
+    it("fails to register without a password", () => {
+      cy.visit("/users/new");
+      cy.get("#username").type("someone");
+      cy.get("#email").type("someone@example.com");
+      cy.get("#submit").click();
+
+      cy.url().should("include", "/users/new");
+
+    });
+  })
 });
